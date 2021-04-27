3afc30529c5da6417c1fad431a674fdc99dafab1
Jing Huang <huangjing427@gmail.com>
2021-04-27 07:05:14 -0400

    done with collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,14 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** A method to get next number in Collatz sequence from the current number*/
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n%2 == 1) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
